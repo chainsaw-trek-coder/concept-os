236c38016871fc110c0530bf354f7fa12775fbd7
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-02-13 04:45:57 +0000

    Updated cross-compiler script.

--- get-cross-compiler.sh
+++ get-cross-compiler.sh
@@ -4,12 +4,23 @@ sudo apt install libmpc-dev -y
 sudo apt install libmpfr-dev -y
 sudo apt install texinfo -y
 pushd /tmp
-wget https://ftp.gnu.org/gnu/gcc/gcc-9.4.0/gcc-9.4.0.tar.gz
-tar xvf gcc-9.4.0.tar.gz
 
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
+
+# Binutils
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.gz
+tar xvf binutils-2.34.tar.gz
+mkdir build-binutils
+cd build-binutils
+../binutils-2.34/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+# GCC
+wget https://ftp.gnu.org/gnu/gcc/gcc-9.4.0/gcc-9.4.0.tar.gz
+tar xvf gcc-9.4.0.tar.gz
 
 which -- $TARGET-as || echo $TARGET-as is not in the PATH
 
