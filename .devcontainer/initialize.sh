e7d9256411cc1c8cb72641abca2a9f2169e85599
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-02-22 01:16:53 +0000

    Updating the devcontainer.

---
+++ .devcontainer/initialize.sh
@@ -0,0 +1,5 @@
+sudo apt update -y
+sudo apt-get install qemu-system -y
+sudo apt install qemu-kvm -y
+
+/workspaces/concept-os/.devcontainer/get-cross-compiler.sh
--- initialize.sh
+++
@@ -1,3 +0,0 @@
-sudo apt update -y
-sudo apt-get install qemu-system -y
-sudo apt install qemu-kvm -y
