2b148fe6bc68c0040a23891785377888cbd2b96f
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-02-22 01:50:38 +0000

    Checkpoint: Helloworld builds.

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo apt update -y
+
+# Grub
+sudo apt install grub2-common -y
+
+# QEMU
 sudo apt-get install qemu-system -y
 sudo apt install qemu-kvm -y
 
--- helloworld/build.sh
+++ helloworld/build.sh
@@ -5,3 +5,8 @@ export PATH="$PREFIX/bin:$PATH"
 i686-elf-as boot.s -o boot.o
 i686-elf-g++ -c kernel.cpp -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
 i686-elf-g++ -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+mkdir -p isodir/boot/grub
+cp myos.bin isodir/boot/myos.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o myos.iso isodir
