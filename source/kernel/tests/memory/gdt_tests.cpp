190da452c8916f6cdc0c86434b1af3f98be535e1
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-02-29 08:35:11 +0000

    Committing my first working test.

--- source/kernel/tests/memory/gdt_tests.cpp
+++ source/kernel/tests/memory/gdt_tests.cpp
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(MemoryTests, GlobalDescriptorTable) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+#if defined(__i386__)
+#include "memory/x86_32/gdt.hpp"
+#endif
+
+TEST(MemoryTests, segment_descriptor_set_type) {
+
+  segment_descriptor descriptor;
+
+  descriptor.set_type(segment_type::execute_read);
+
+  auto type_mask = 0xF00;
+  auto dword1 = descriptor.dword1 & type_mask;
+
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_EQ(dword1, 0xA00);
 }
