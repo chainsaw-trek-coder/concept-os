0b376c226fed96dfca5e67330c8e59a75b5fc8e9
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-03-01 01:20:33 +0000

    Started paging data structures. Added gdt base address test.

--- source/kernel/tests/memory/gdt_tests.cpp
+++ source/kernel/tests/memory/gdt_tests.cpp
@@ -4,7 +4,7 @@
 #include "memory/x86_32/gdt.hpp"
 #endif
 
-TEST(MemoryTests, segment_descriptor_set_type) {
+TEST(MemoryTests, segment_descriptor_can_set_type) {
 
   segment_descriptor descriptor;
 
@@ -13,6 +13,22 @@ TEST(MemoryTests, segment_descriptor_set_type) {
   auto type_mask = 0xF00;
   auto dword1 = descriptor.dword1 & type_mask;
 
-  // Expect equality.
   EXPECT_EQ(dword1, 0xA00);
+}
+
+TEST(MemoryTests, segment_descriptor_can_set_base_address) {
+
+  segment_descriptor descriptor;
+
+  unsigned address = 0xBAADF00D;
+
+  descriptor.set_base_address(reinterpret_cast<void*>(address));
+
+  auto address_part_1_mask = 0xFF000000;
+  auto address_part_2_mask = 0x000000FF;
+  auto address_part_3_mask = 0xFFFF0000;
+
+  EXPECT_EQ(descriptor.dword1 & address_part_1_mask, address & address_part_1_mask);
+  EXPECT_EQ(descriptor.dword1 & address_part_2_mask, (address >> 16) & address_part_2_mask);
+  EXPECT_EQ(descriptor.dword2 & address_part_3_mask, (address << 16) & address_part_3_mask);
 }
