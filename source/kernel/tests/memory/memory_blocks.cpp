3696572b2b8fb6f2859e96dcb0aa1393bbe3627f
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-04-25 02:16:28 +0000

    Checkpoint: Can allocate two blocks.

--- source/kernel/tests/memory/memory_blocks.cpp
+++ source/kernel/tests/memory/memory_blocks.cpp
@@ -3,18 +3,63 @@
 
 TEST(MemoryTests, blocks_can_initialize_free_blocks)
 {
-    unsigned char memory[4096*16];
+    auto initial_size = 4096*16;
+    unsigned char memory[initial_size];
 
     for(unsigned i = 0; i < 4096 * 16; i++)
         memory[i] = static_cast<unsigned char>(0xFF);
 
     memory_blocks _memory_blocks;
-    _memory_blocks.initialize(reinterpret_cast<void*>(memory), 4096*16);
+    _memory_blocks.initialize(reinterpret_cast<void*>(memory), initial_size);
 
     auto first_free_block = reinterpret_cast<free_block*>(memory);
 
+    EXPECT_EQ(first_free_block, _memory_blocks.free_blocks);
     EXPECT_EQ(_memory_blocks.size, 4096 * 16);
     EXPECT_EQ(first_free_block->size, 4096 * 16);
     EXPECT_EQ(first_free_block->smaller_block, nullptr);
     EXPECT_EQ(first_free_block->larger_block, nullptr);
+}
+
+TEST(MemoryTests, blocks_can_allocate_a_block)
+{
+    auto initial_size = 4096*16;
+    unsigned char memory[initial_size];
+
+    for(unsigned i = 0; i < 4096 * 16; i++)
+        memory[i] = static_cast<unsigned char>(0xFF);
+
+    memory_blocks _memory_blocks;
+    _memory_blocks.initialize(reinterpret_cast<void*>(memory), initial_size);
+
+    auto allocated_block = _memory_blocks.allocate(4096);
+
+    EXPECT_EQ(_memory_blocks.free_blocks->size, initial_size - 4096);
+    EXPECT_NE(reinterpret_cast<void*>(_memory_blocks.free_blocks), allocated_block);
+    EXPECT_EQ(_memory_blocks.free_blocks->predecessor_by_size, nullptr);
+    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block, nullptr);
+    EXPECT_EQ(_memory_blocks.free_blocks->larger_block, nullptr);
+}
+
+TEST(MemoryTests, blocks_can_allocate_two_blocks)
+{
+    auto initial_size = 4096*16;
+    unsigned char memory[initial_size];
+
+    for(unsigned i = 0; i < 4096 * 16; i++)
+        memory[i] = static_cast<unsigned char>(0xFF);
+
+    memory_blocks _memory_blocks;
+    _memory_blocks.initialize(reinterpret_cast<void*>(memory), initial_size);
+
+    auto allocated_block_1 = _memory_blocks.allocate(4096);
+    auto allocated_block_2 = _memory_blocks.allocate(4096);
+
+    EXPECT_EQ(_memory_blocks.free_blocks->size, initial_size - 4096 - 4096);
+    EXPECT_NE(reinterpret_cast<void*>(_memory_blocks.free_blocks), allocated_block_1);
+    EXPECT_NE(reinterpret_cast<void*>(_memory_blocks.free_blocks), allocated_block_2);
+    EXPECT_NE(allocated_block_1, allocated_block_2);
+    EXPECT_EQ(_memory_blocks.free_blocks->predecessor_by_size, nullptr);
+    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block, nullptr);
+    EXPECT_EQ(_memory_blocks.free_blocks->larger_block, nullptr);
 }
