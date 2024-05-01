2c15a49a2ca50948fe8d6fd95912ba20c6d1f6b4
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-05-01 06:57:32 +0000

    Better memory validation.

--- source/kernel/tests/memory/memory_blocks.cpp
+++ source/kernel/tests/memory/memory_blocks.cpp
@@ -1,5 +1,45 @@
 #include <gtest/gtest.h>
 #include "memory/memory_blocks.hpp"
+#include <algorithm>
+#include <vector>
+
+void validate_free_blocks(free_block *root, std::vector<free_block *> &path, unsigned &free_size)
+{
+    if (root == nullptr)
+        return;
+
+    auto has_visited = std::find(path.begin(), path.end(), root) != path.end();
+
+    EXPECT_FALSE(has_visited);
+
+    path.push_back(root);
+    free_size += root->size;
+
+    if (root->smaller_block != nullptr)
+    {
+        EXPECT_TRUE(root->size >= root->smaller_block->size);
+        EXPECT_EQ(root, root->smaller_block->predecessor_by_size);
+        validate_free_blocks(root->smaller_block, path, free_size);
+    }
+
+    if (root->larger_block != nullptr)
+    {
+        EXPECT_TRUE(root->size <= root->larger_block->size);
+        EXPECT_EQ(root, root->larger_block->predecessor_by_size);
+        validate_free_blocks(root->larger_block, path, free_size);
+    }
+
+    path.pop_back();
+}
+
+void validate_free_blocks(free_block *root, unsigned expected_free_size)
+{
+    std::vector<free_block *> path;
+    unsigned free_size = 0;
+    validate_free_blocks(root, path, free_size);
+
+    EXPECT_EQ(expected_free_size, free_size);
+}
 
 TEST(MemoryTests, blocks_can_initialize_free_blocks)
 {
@@ -111,167 +151,34 @@ TEST(MemoryTests, blocks_can_randomly_allocate_and_deallocate)
 
     auto allocated_block_1 = _memory_blocks.allocate(4096);
     // Tree should have on smaller block.
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 15);
 
     auto allocated_block_2 = _memory_blocks.allocate(4096);
     // Tree should have yet an even smaller block.
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 14);
 
     auto allocated_block_3 = _memory_blocks.allocate(4096);
     // Tree should have yet an even smaller block.
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 13);
 
     _memory_blocks.deallocate(allocated_block_1);
-    /* Tree should look like this...
-
-                13 blocks
-            /
-
-        1 block
-
-    */
-
-    EXPECT_EQ(_memory_blocks.free_blocks->size, 4096 * 13);
-    EXPECT_EQ(_memory_blocks.free_blocks->larger_block, nullptr);
-    EXPECT_NE(_memory_blocks.free_blocks->smaller_block, nullptr);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->size, 4096 * 1);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block, nullptr);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block, nullptr);
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 14);
 
     _memory_blocks.deallocate(allocated_block_3);
-    /* Tree should look like this...
-
-                13 blocks
-            /
-
-        1 block
-            |
-        1 block
-
-    */
-    EXPECT_EQ(_memory_blocks.free_blocks->size, 4096 * 13);
-    EXPECT_EQ(_memory_blocks.free_blocks->larger_block, nullptr);
-    EXPECT_NE(_memory_blocks.free_blocks->smaller_block, nullptr);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->size, 4096 * 1);
-    EXPECT_TRUE(_memory_blocks.free_blocks->smaller_block->smaller_block == nullptr || _memory_blocks.free_blocks->smaller_block->larger_block == nullptr);
-    EXPECT_FALSE(_memory_blocks.free_blocks->smaller_block->smaller_block == nullptr && _memory_blocks.free_blocks->smaller_block->larger_block == nullptr);
-
-    if (_memory_blocks.free_blocks->smaller_block->smaller_block != nullptr)
-    {
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->size, 4096 * 1);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->smaller_block, nullptr);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->larger_block, nullptr);
-    }
-
-    if (_memory_blocks.free_blocks->smaller_block->larger_block != nullptr)
-    {
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->size, 4096 * 1);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->smaller_block, nullptr);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->larger_block, nullptr);
-    }
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 15);
 
     _memory_blocks.deallocate(allocated_block_2);
-    /* Tree should look like this...
-
-                9 blocks
-            /
-
-        1 block
-            |
-        1 block
-            |
-        1 block
-
-    */
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 16);
 
     allocated_block_1 = _memory_blocks.allocate(4096);
-    /* Tree should look like this...
-
-                13 blocks
-            /
-
-        1 block
-            |
-        1 block
-
-    */
-    EXPECT_EQ(_memory_blocks.free_blocks->size, 4096 * 13);
-    EXPECT_EQ(_memory_blocks.free_blocks->larger_block, nullptr);
-    EXPECT_NE(_memory_blocks.free_blocks->smaller_block, nullptr);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->size, 4096 * 1);
-    EXPECT_TRUE(_memory_blocks.free_blocks->smaller_block->smaller_block == nullptr || _memory_blocks.free_blocks->smaller_block->larger_block == nullptr);
-    EXPECT_FALSE(_memory_blocks.free_blocks->smaller_block->smaller_block == nullptr && _memory_blocks.free_blocks->smaller_block->larger_block == nullptr);
-
-    if (_memory_blocks.free_blocks->smaller_block->smaller_block != nullptr)
-    {
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->size, 4096 * 1);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->smaller_block, nullptr);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->larger_block, nullptr);
-    }
-
-    if (_memory_blocks.free_blocks->smaller_block->larger_block != nullptr)
-    {
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->size, 4096 * 1);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->smaller_block, nullptr);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->larger_block, nullptr);
-    }
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 15);
 
     allocated_block_2 = _memory_blocks.allocate(4096);
-    /* Tree should look like this...
-
-                13 blocks
-            /
-
-        1 block
-
-    */
-    EXPECT_EQ(_memory_blocks.free_blocks->size, 4096 * 13);
-    EXPECT_EQ(_memory_blocks.free_blocks->larger_block, nullptr);
-    EXPECT_NE(_memory_blocks.free_blocks->smaller_block, nullptr);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->size, 4096 * 1);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block, nullptr);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block, nullptr);
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 14);
 
     _memory_blocks.deallocate(allocated_block_2);
-    /* Tree should look like this...
-
-                13 blocks
-            /
-
-        1 block
-            |
-        1 block
-
-    */
-    EXPECT_EQ(_memory_blocks.free_blocks->size, 4096 * 13);
-    EXPECT_EQ(_memory_blocks.free_blocks->larger_block, nullptr);
-    EXPECT_NE(_memory_blocks.free_blocks->smaller_block, nullptr);
-    EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->size, 4096 * 1);
-    EXPECT_TRUE(_memory_blocks.free_blocks->smaller_block->smaller_block == nullptr || _memory_blocks.free_blocks->smaller_block->larger_block == nullptr);
-    EXPECT_FALSE(_memory_blocks.free_blocks->smaller_block->smaller_block == nullptr && _memory_blocks.free_blocks->smaller_block->larger_block == nullptr);
-
-    if (_memory_blocks.free_blocks->smaller_block->smaller_block != nullptr)
-    {
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->size, 4096 * 1);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->smaller_block, nullptr);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->smaller_block->larger_block, nullptr);
-    }
-
-    if (_memory_blocks.free_blocks->smaller_block->larger_block != nullptr)
-    {
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->size, 4096 * 1);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->smaller_block, nullptr);
-        EXPECT_EQ(_memory_blocks.free_blocks->smaller_block->larger_block->larger_block, nullptr);
-    }
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 15);
 
     _memory_blocks.deallocate(allocated_block_1);
-    /* Tree should look like this...
-
-                13 blocks
-            /
-
-        1 block
-            |
-        1 block
-            |
-        1 block
-
-    */
+    validate_free_blocks(_memory_blocks.free_blocks, 4096 * 16);
 }
