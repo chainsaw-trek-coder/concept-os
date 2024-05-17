3a4fa11c40bb83bcb504be4e2ca9fb7e7b2fa9b9
Brian M <118866349+b-rain-m@users.noreply.github.com>
2024-05-17 05:16:46 +0000

    Checkpoint: Does not pass 1 thousand memory operations.

--- source/kernel/source/memory/memory_blocks.cpp
+++ source/kernel/source/memory/memory_blocks.cpp
@@ -41,6 +41,13 @@ void memory_blocks::remove_node_from_tree(free_block *block)
 {
     remove_node_from_tree_by_size(block);
     remove_node_from_tree_by_address(block);
+    block->predecessor_by_size = nullptr;
+    block->predecessor_by_address = nullptr;
+    block->lower_block = nullptr;
+    block->higher_block = nullptr;
+    block->smaller_block = nullptr;
+    block->larger_block = nullptr;
+    size -= block->size;
 }
 
 void memory_blocks::remove_node_from_tree_by_size(free_block *block)
@@ -56,7 +63,7 @@ void memory_blocks::remove_node_from_tree_by_size(free_block *block)
     if (block->smaller_block == nullptr && block->larger_block != nullptr)
     {
         block->larger_block->predecessor_by_size = block->predecessor_by_size;
-        replace_block_at_its_predecessor_by_size(block, block->larger_block);        
+        replace_block_at_its_predecessor_by_size(block, block->larger_block);
         return;
     }
 
@@ -81,24 +88,22 @@ void memory_blocks::remove_node_from_tree_by_size(free_block *block)
     if (successor->larger_block != nullptr)
         successor->predecessor_by_size->smaller_block = successor->larger_block;
 
-    successor->predecessor_by_size = nullptr;
-
     // Link it in the same way block is linked.
     replace_block_at_its_predecessor_by_size(block, successor);
 
     successor->predecessor_by_size = block->predecessor_by_size;
-    
-    if(block->smaller_block != successor)
+
+    if (block->smaller_block != successor)
         successor->smaller_block = block->smaller_block;
-    
-    if(block->larger_block != successor)
+
+    if (block->larger_block != successor)
         successor->larger_block = block->larger_block; // Given the algorithm probably should never happen.
 
-    if (block->larger_block != nullptr)
-        block->larger_block->predecessor_by_size = successor;
+    if (successor->larger_block != nullptr)
+        successor->larger_block->predecessor_by_size = successor;
 
-    if (block->smaller_block != nullptr)
-        block->smaller_block->predecessor_by_size = successor;
+    if (successor->smaller_block != nullptr)
+        successor->smaller_block->predecessor_by_size = successor;
 }
 
 void memory_blocks::remove_node_from_tree_by_address(free_block *block)
@@ -114,7 +119,7 @@ void memory_blocks::remove_node_from_tree_by_address(free_block *block)
     if (block->lower_block == nullptr && block->higher_block != nullptr)
     {
         block->higher_block->predecessor_by_address = block->predecessor_by_address;
-        replace_block_at_its_predecessor_by_address(block, block->higher_block);        
+        replace_block_at_its_predecessor_by_address(block, block->higher_block);
         return;
     }
 
@@ -137,32 +142,38 @@ void memory_blocks::remove_node_from_tree_by_address(free_block *block)
 
     // Make sure to remove block from tree.
     if (successor->higher_block != nullptr)
-        successor->predecessor_by_address->lower_block = successor->higher_block;
+    {
+        if (successor->predecessor_by_address->lower_block == successor)
+            successor->predecessor_by_address->lower_block = successor->higher_block;
+        else if (successor->predecessor_by_address->higher_block == successor)
+            successor->predecessor_by_address->higher_block = successor->higher_block;
 
-    successor->predecessor_by_address = nullptr;
+        successor->higher_block->predecessor_by_address = successor->predecessor_by_address;
+    }
 
     // Link it in the same way block is linked.
     replace_block_at_its_predecessor_by_address(block, successor);
 
     successor->predecessor_by_address = block->predecessor_by_address;
-    
-    if(block->lower_block != successor)
+
+    if (block->lower_block != successor)
         successor->lower_block = block->lower_block;
-    
-    if(block->higher_block != successor)
+
+    if (block->higher_block != successor)
         successor->higher_block = block->higher_block; // Given the algorithm probably should never happen.
 
-    if (block->higher_block != nullptr)
-        block->higher_block->predecessor_by_address = successor;
+    if (successor->higher_block != nullptr)
+        successor->higher_block->predecessor_by_address = successor;
 
-    if (block->lower_block != nullptr)
-        block->lower_block->predecessor_by_address = successor;
+    if (successor->lower_block != nullptr)
+        successor->lower_block->predecessor_by_address = successor;
 }
 
 void memory_blocks::add_node_to_tree(free_block *block)
 {
     add_node_to_tree_by_size(block);
     add_node_to_tree_by_address(block);
+    size += block->size;
 }
 
 void memory_blocks::add_node_to_tree_by_size(free_block *block)
@@ -255,7 +266,7 @@ void memory_blocks::add_node_to_tree_by_address(free_block *block)
             }
         }
 
-        if(block == current)
+        if (block == current)
         {
             // What are we doing here. Block is already on the tree.
             break;
@@ -266,7 +277,7 @@ void memory_blocks::add_node_to_tree_by_address(free_block *block)
 void *memory_blocks::allocate(unsigned size_in_bytes)
 {
     // No more memory...
-    if(free_blocks == nullptr)
+    if (free_blocks == nullptr)
         return nullptr;
 
     // Find suitable block.
@@ -344,18 +355,57 @@ void *memory_blocks::allocate_next_to(void *address, unsigned size_in_bytes)
 void memory_blocks::deallocate(void *address)
 {
     // Setup block as a free data structure.
-    auto new_block = reinterpret_cast<free_block*>(address);
+    auto new_block = reinterpret_cast<free_block *>(address);
     new_block->predecessor_by_size = nullptr;
     new_block->predecessor_by_address = nullptr;
     new_block->smaller_block = nullptr;
     new_block->larger_block = nullptr;
     new_block->lower_block = nullptr;
     new_block->higher_block = nullptr;
-    
     new_block->size = 4096;
 
+    // Merge new bock into adjacent blocks...
+    auto current = free_blocks_by_address;
+
+    while (current)
+    {
+        // If we are at block that is adjacent... merge.
+
+        // Check if current is to the left of the new block.
+        if ((reinterpret_cast<unsigned char *>(current) + current->size) == reinterpret_cast<unsigned char *>(new_block))
+        {
+            // Merge...
+            remove_node_from_tree(current);
+            current->size += new_block->size;
+            new_block = current;
+            current = free_blocks_by_address; // Reset search...
+            continue;
+        }
+
+        // Check if current is to the right of the new block.
+        if ((reinterpret_cast<unsigned char *>(new_block) + new_block->size) == reinterpret_cast<unsigned char *>(current))
+        {
+            // Merge...
+            remove_node_from_tree(current);
+            new_block->size += current->size;
+            current = free_blocks_by_address; // Reset search...
+            continue;
+        }
+
+        if (current < new_block)
+        {
+            current = current->higher_block;
+            continue;
+        }
+
+        if (current > new_block)
+        {
+            current = current->lower_block;
+            continue;
+        }
+    }
+
     add_node_to_tree(new_block);
-    size += 4096;
 
     // TODO: Finish structuring tree by address so that merging is possible.
 
--- source/kernel/tests/memory/memory_blocks.cpp
+++ source/kernel/tests/memory/memory_blocks.cpp
@@ -16,6 +16,9 @@ void validate_free_blocks_by_size(free_block *root, vector<free_block *> &visite
 
     EXPECT_FALSE(has_visited);
 
+    if(has_visited)
+        return; // Don't want to get caught up in an infinite recursion.
+
     visited.push_back(root);
     free_size += root->size;
 
@@ -42,6 +45,9 @@ void validate_free_blocks_by_address(free_block *root, vector<free_block *> &vis
     auto has_visited = find(visited.begin(), visited.end(), root) != visited.end();
 
     EXPECT_FALSE(has_visited);
+
+    if(has_visited)
+        return; // Don't want to get caught up in an infinite recursion.
 
     visited.push_back(root);
     free_size += root->size;
@@ -213,6 +219,7 @@ TEST(MemoryTests, blocks_can_simply_allocate_and_deallocate)
     allocated_block_2 = _memory_blocks.allocate(4096);
     validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, 4096 * 14);
 
+
     _memory_blocks.deallocate(allocated_block_2);
     validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, 4096 * 15);
 
@@ -267,10 +274,279 @@ TEST(MemoryTests, blocks_can_randomly_allocate_and_deallocate)
         // Deallocate
         case 1:
             if (blocks_allocated > 0)
-            {
-                cout << "Deallocating block..." << endl;
+            {                
                 auto block_to_deallocate_position = deallocate_dist(random) % blocks.size();
                 auto block_to_deallocate = blocks[block_to_deallocate_position];
+
+                cout << "Deallocating block " << block_to_deallocate_position << "..." << endl;
+
+                blocks.erase(find(blocks.begin(), blocks.end(), block_to_deallocate));
+                _memory_blocks.deallocate(block_to_deallocate);
+                blocks_allocated--;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+
+            // Note if we attempt to deallocate what doesn't exist... we should end up with memory corruption.
+            break;
+        }
+    }
+}
+
+TEST(MemoryTests, blocks_can_randomly_allocate_and_deallocate_1thousand_times)
+{
+    auto num_blocks = 1024;
+    auto initial_size = 4096 * num_blocks;
+    unsigned char memory[initial_size];
+
+    for (unsigned i = 0; i < 4096 * num_blocks; i++)
+        memory[i] = static_cast<unsigned char>(0xFF);
+
+    memory_blocks _memory_blocks;
+    _memory_blocks.initialize(reinterpret_cast<void *>(memory), initial_size);
+    // Tree is one big block.
+
+    vector<void *> blocks;
+
+    random_device random;
+    uniform_int_distribution<int> operation_dist(0, 1);
+    uniform_int_distribution<int> deallocate_dist(0, num_blocks);
+
+    unsigned blocks_allocated = 0;
+
+    for (auto i = 0; i < 1000; i++)
+    {
+        auto operation = operation_dist(random);
+
+        switch (operation)
+        {
+        // Allocate
+        case 0:
+            if (blocks_allocated < 16)
+            {
+                cout << "Allocating block..." << endl;
+                blocks.push_back(_memory_blocks.allocate(4096));                
+                blocks_allocated++;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            else
+            {
+                cout << "Allocated block while out of memory..." << endl;
+                EXPECT_EQ(_memory_blocks.allocate(4096), nullptr);
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            break;
+
+        // Deallocate
+        case 1:
+            if (blocks_allocated > 0)
+            {                
+                auto block_to_deallocate_position = deallocate_dist(random) % blocks.size();
+                auto block_to_deallocate = blocks[block_to_deallocate_position];
+
+                cout << "Deallocating block " << block_to_deallocate_position << "..." << endl;
+
+                blocks.erase(find(blocks.begin(), blocks.end(), block_to_deallocate));
+                _memory_blocks.deallocate(block_to_deallocate);
+                blocks_allocated--;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+
+            // Note if we attempt to deallocate what doesn't exist... we should end up with memory corruption.
+            break;
+        }
+    }
+}
+
+TEST(MemoryTests, blocks_can_allocate_and_deallocate_failure_scenario_1)
+{
+    auto num_blocks = 16;
+    auto initial_size = 4096 * num_blocks;
+    unsigned char memory[initial_size];
+
+    for (unsigned i = 0; i < 4096 * num_blocks; i++)
+        memory[i] = static_cast<unsigned char>(0xFF);
+
+    memory_blocks _memory_blocks;
+    _memory_blocks.initialize(reinterpret_cast<void *>(memory), initial_size);
+    // Tree is one big block.
+
+    vector<void *> blocks;
+
+    int operations[32] = { 1, 1, 1, 1, 1, -2, -3, 1, -2, 1, 1, -4, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, -2, -3, 0, 0, 1, -1, 1, -1 ,0 };
+
+    unsigned blocks_allocated = 0;
+
+    for (auto i = 0; i < 32; i++)
+    {
+        auto operation = operations[i];
+
+        switch (operation)
+        {
+        // Allocate
+        case 1:
+            if (blocks_allocated < 16)
+            {
+                cout << "Allocating block..." << endl;
+                blocks.push_back(_memory_blocks.allocate(4096));                
+                blocks_allocated++;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            else
+            {
+                cout << "Allocated block while out of memory..." << endl;
+                EXPECT_EQ(_memory_blocks.allocate(4096), nullptr);
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            break;
+
+        // Deallocate
+        default:
+            if (blocks_allocated > 0)
+            {                
+                auto block_to_deallocate_position = operations[i] * -1;
+                auto block_to_deallocate = blocks[block_to_deallocate_position];
+
+                cout << "Deallocating block " << block_to_deallocate_position << "..." << endl;
+
+                blocks.erase(find(blocks.begin(), blocks.end(), block_to_deallocate));
+                _memory_blocks.deallocate(block_to_deallocate);
+                blocks_allocated--;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+
+            // Note if we attempt to deallocate what doesn't exist... we should end up with memory corruption.
+            break;
+        }
+    }
+}
+
+TEST(MemoryTests, blocks_can_allocate_and_deallocate_failure_scenario_2)
+{
+    auto num_blocks = 16;
+    auto initial_size = 4096 * num_blocks;
+    unsigned char memory[initial_size];
+
+    for (unsigned i = 0; i < 4096 * num_blocks; i++)
+        memory[i] = static_cast<unsigned char>(0xFF);
+
+    memory_blocks _memory_blocks;
+    _memory_blocks.initialize(reinterpret_cast<void *>(memory), initial_size);
+    // Tree is one big block.
+
+    vector<void *> blocks;
+
+    int operations[27] = { 1, 1, 1, 1, 1, -1, -1, 1, 1, -3, 1, 0, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 0, -1, -3, -5, 0 };
+
+    unsigned blocks_allocated = 0;
+
+    for (auto i = 0; i < 27; i++)
+    {
+        auto operation = operations[i];
+
+        switch (operation)
+        {
+        // Allocate
+        case 1:
+            if (blocks_allocated < 16)
+            {
+                cout << "Allocating block..." << endl;
+                blocks.push_back(_memory_blocks.allocate(4096));                
+                blocks_allocated++;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            else
+            {
+                cout << "Allocated block while out of memory..." << endl;
+                EXPECT_EQ(_memory_blocks.allocate(4096), nullptr);
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            break;
+
+        // Deallocate
+        default:
+            if (blocks_allocated > 0)
+            {                
+                auto block_to_deallocate_position = operations[i] * -1;
+                auto block_to_deallocate = blocks[block_to_deallocate_position];
+
+                cout << "Deallocating block " << block_to_deallocate_position << "..." << endl;
+
+                blocks.erase(find(blocks.begin(), blocks.end(), block_to_deallocate));
+                _memory_blocks.deallocate(block_to_deallocate);
+                blocks_allocated--;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+
+            // Note if we attempt to deallocate what doesn't exist... we should end up with memory corruption.
+            break;
+        }
+    }
+}
+
+TEST(MemoryTests, blocks_can_allocate_and_deallocate_failure_scenario_3)
+{
+    auto num_blocks = 16;
+    auto initial_size = 4096 * num_blocks;
+    unsigned char memory[initial_size];
+
+    for (unsigned i = 0; i < 4096 * num_blocks; i++)
+        memory[i] = static_cast<unsigned char>(0xFF);
+
+    memory_blocks _memory_blocks;
+    _memory_blocks.initialize(reinterpret_cast<void *>(memory), initial_size);
+    // Tree is one big block.
+
+    vector<void *> blocks;
+
+    vector<int> operations = {
+        1, 1, 1, -2, 1, 1, -2, 1, -1, -1, 1, -2, 0, 0, 1, 1, -1, 1, -1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+        1, 0, 1, 0, 1, 1, 1, 1, 1, -3, -3, 0, 1, 1, -1, -2, 1, -2, 1, -2, 0, 1, 0, 0, 1, 1, 1, -2, 1, 1, 
+        1, 1, -1, -3, 1, -3, -2, 1, -3, -2, 1, -1, 1, -2, -1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, -4, -4, 1,
+        -2, -2, -2, -1, 1, 1, -2, 1, -1, -1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, -3, -2, 1, 0, 0, 0, 
+        1, 1, 1, 1, 1, -3, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, -7, -3, 1, 1, 0, -2, -1, 1, 1, 1, -8, 1, -4,
+        -9, -9, -5, 1, -4, 1, -1, 1, 1, -2, 1, -6, -3, 1, 1, -1, -1, -4, 1, 1, 0, -4, 1, 1, -2, -4, -5,
+        1, 1, 1, 1, -1, 1, 0, 1, 1, -6, -7, 1, 1, -6, -8, 1, -10, 1, -5, -8, -1, 1, -3, 1, -1, 1, -2, 1, 
+        -8, -7, 1, 1, -7, 1, 1, 1, 1, -2, -4, -1, -5, -1, 1, 0, -1, 1, 1, 0, 1, 1, 0, 1, 1, 1, -9, 1, 1,
+        1, -7, -8, 1, -5, -7, -5, 1, 1, 1, 1, -7, -7, 1, -8, -10, -8, -3, 1, 1, 1, 1, 1, -7, 1, 1, -1, 1,
+        -1, -1, -11, -4, 1, 1, 1, 1, -7, -1, -9, 1, 1, -1, 1, 1, -13, 1, -12, -12, 1, -1, 1, -10, -11, -1,
+        1, 1, 1, 1, -3, 1, -1, 1, -7, -12, -5, 0, 1, 1, -5, -3, 1, -7, -3, -6, -6, 1, 1, 1, -1, 1, -6, -8,
+        1, -2, 1, -4, 1, 1, -4, -3, -3, -4, -4, 1, -5, 1
+    };
+
+    unsigned blocks_allocated = 0;
+
+    for (auto i = 0; i < operations.size(); i++)
+    {
+        auto operation = operations[i];
+
+        switch (operation)
+        {
+        // Allocate
+        case 1:
+            if (blocks_allocated < 16)
+            {
+                cout << "Allocating block..." << endl;
+                blocks.push_back(_memory_blocks.allocate(4096));                
+                blocks_allocated++;
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            else
+            {
+                cout << "Allocated block while out of memory..." << endl;
+                EXPECT_EQ(_memory_blocks.allocate(4096), nullptr);
+                validate_free_blocks(_memory_blocks.free_blocks, _memory_blocks.free_blocks_by_address, (num_blocks - blocks_allocated) * 4096);
+            }
+            break;
+
+        // Deallocate
+        default:
+            if (blocks_allocated > 0)
+            {                
+                auto block_to_deallocate_position = (operations[i] * -1) % blocks.size();
+                auto block_to_deallocate = blocks[block_to_deallocate_position];
+
+                cout << "Deallocating block " << block_to_deallocate_position << "..." << endl;
+
                 blocks.erase(find(blocks.begin(), blocks.end(), block_to_deallocate));
                 _memory_blocks.deallocate(block_to_deallocate);
                 blocks_allocated--;
